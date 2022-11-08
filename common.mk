15b4191504ed6c01a22aaf16694126f8f31398d1
johnmart19 <johnivan19999@gmail.com>
2022-11-08 03:55:31 +0200

    sm8350-common: Import and adapt Xiaomi VibratorFeature
    This change includes:
    - Sepolicy ports from Miui for Xiaomi Vibrator and VibratorFeature hals
    - Import needed Prebuilts of AIDL service and required deps

--- common.mk
+++ common.mk
@@ -547,6 +547,19 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Vibrator Configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
+# Vibrator Xiaomi
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-V1-ndk.vendor
+
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so
+
 # VNDK
 # vndservicemanager has been removed from API30 devices (aosp/1235751)
 # but we still need it for display services.
