ec81a9a305cc936f090573b39b909d6b5088a074
New Author Name <johnivan19999@gmail.com>
2024-03-04 19:05:31 +0530

    sm8350-common: Remove common QTI vibrator HAL
    - Removes 468c33c5f3c4b17768a829eacb86b30be2e53a21

--- common.mk
+++ common.mk
@@ -547,13 +547,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
-
 # VNDK
 # vndservicemanager has been removed from API30 devices (aosp/1235751)
 # but we still need it for display services.
