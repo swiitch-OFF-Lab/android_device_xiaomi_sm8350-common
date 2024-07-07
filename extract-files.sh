31589948443b4a655339a5cc0a4c7c32a42d0add
flakeforever <113103193+flakeforever@users.noreply.github.com>
2024-07-07 17:37:23 +0800

    sm8350-common: Build libcodec2_hidl_shim

--- extract-files.sh
+++ extract-files.sh
@@ -85,16 +85,8 @@ function blob_fixup() {
        vendor/bin/hw/dolbycodec2 | vendor/bin/hw/vendor.qti.media.c2@1.0-service)     
             "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
-        vendor/lib/libcodec2_hidl@1.0_vendor.so)
-            "${PATCHELF}" --set-soname "libcodec2_hidl@1.0_vendor.so" "${2}"
-            "${PATCHELF}" --replace-needed "libcodec2_vndk.so" "libcodec2_vndk_vendor.so" "${2}"
-            ;;
-        vendor/lib/libcodec2_vndk_vendor.so)
-            "${PATCHELF}" --set-soname "libcodec2_vndk_vendor.so" "${2}"
-            ;;
         vendor/lib/c2.dolby.client.so)
-            "${PATCHELF}" --replace-needed "libcodec2_vndk.so" "libcodec2_vndk_vendor.so" "${2}"
-            "${PATCHELF}" --replace-needed "libcodec2_hidl@1.0.so" "libcodec2_hidl@1.0_vendor.so" "${2}"
+            "${PATCHELF}" --add-needed "libcodec2_hidl_shim.so" "${2}"
             ;;
     esac
 }
